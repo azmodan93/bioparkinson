429e27e16d2f53b7227c519417307b5c4e282b9b
Agustin Di Prinzio <agus.diprinzio@gmail.com>
2017-10-29 14:22:34 -0300

    termino el ejercicio 1 con comentarios pipicucu

--- exercise1.pl
+++ exercise1.pl
@@ -13,5 +13,14 @@ my $outputPath = "Exercise1/Output/orfs.fasta";
 
 my $factory = new Bio::Factory::EMBOSS;
 my $app = $factory->program("getorf");
-my %input = ( -sequence => $inputPath, -find => 1, -reverse => 1, -minsize => 300, -outseq => $outputPath);
+my %input = ( -sequence => $inputPath, -find => 1, -reverse => 1, -minsize => 300, -outseq => "orfs.fasta");
 $app->run(\%input);
+
+=begin HowToRun + Explanation
+Prerrequisitos:		Tener instalado Perl
+					Tener instalado el paquete Bio::Factory::Emboss - 
+					Puede hacerse mediante el gestor de paquetes Cpan con el comando force install Bio::Factory::Emboss
+Para ejecutar el script se debe correr el comando perl exercise1.pl
+Para resolver este problema, se utilizó la herramienta Emboss que lo que realiza es traducir un gen que recibe como input, con todas sus posibles orfs
+tomando como tamaño mínimo el indicado por parámetro. Dicho resultado lo almacena en el archivo que recible por parámetro.
+=cut
